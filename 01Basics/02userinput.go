1c0ad671f65e67c66d970205029615b96e479f81
Padmaja Bhol <75530516+padmajabhol@users.noreply.github.com>
2021-11-07 00:35:05 +0530

    userinputs

--- 01Basics/02userinput.go
+++ 01Basics/02userinput.go
@@ -1,15 +1,28 @@
 package main
 
-import {
-	"fmt"
+import (
+	// "fmt"
 	"bufio"
+	"fmt"
 	"os"
 	"strconv"
 	"strings"
-}
+)
 
 func main(){
 
-	var name string = "padmaja"
-	fmt.Println((name))
+	// var name string = "padmaja"
+	// var a, _ = fmt.Println(name)
+	// fmt.Println(a)
+
+	// reader := bufio.NewReader(os.Stdin)
+	// fmt.Print("Enter your full name: ")
+	// myname, _ := reader.ReadString('\n')
+	// fmt.Println((myname))
+
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Enter your rating: ")
+	myrating, _ := reader.ReadString('\n')
+	mynumRating, _ := strconv.ParseFloat(strings.TrimSpace(myrating), 64)
+	fmt.Println(mynumRating + 2)
 }
